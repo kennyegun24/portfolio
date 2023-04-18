6ccebf18ab90af889577d5bccbfb21e062031730
Kenny Elias <109461921+kennyegun24@users.noreply.github.com>
2023-04-18 08:10:06 +0100

    Modify

--- src/components/head/Head.jsx
+++ src/components/head/Head.jsx
@@ -9,8 +9,8 @@ const Head = () => {
                 <h1 className='headDtls'>Full Stack Developer Ruby on Rails and <br /> Front-end React.Js Developer</h1>
                 <p className='headerCreate'>I create beautiful and fully functional webapps</p>
                 <div className='icons flex'>
-                    <a href='https://github.com/kennyegun24'><FaGithub /></a>
-                    <a href='https://linkedin.com/in/kenny-egun-kennyegun24'><FaLinkedin /></a>
+                    <a href='https://github.com/kennyegun24' target="_blank"><FaGithub /></a>
+                    <a href='https://linkedin.com/in/kenny-egun-kennyegun24' target="_blank"><FaLinkedin /></a>
                 </div>
             </div>
         </div>
